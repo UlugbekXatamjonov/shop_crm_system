f8de3db6cd959a816194fc08956c01b382e00ac9
UlugbekXatamjonov <xatamjonovulugbek17@gmail.com>
2026-02-11 17:19:36 +0500

    login/logout uchun API

--- accaunt/serializers.py
+++ accaunt/serializers.py
@@ -1,191 +1,180 @@
-# from django.utils.encoding import smart_str, force_bytes, DjangoUnicodeDecodeError
-# from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
-# from django.contrib.auth.tokens import PasswordResetTokenGenerator
-# from django.contrib.auth.password_validation import validate_password
-# from django.contrib.auth import authenticate
+from django.utils.encoding import smart_str, force_bytes, DjangoUnicodeDecodeError
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import authenticate
 
-# from rest_framework import serializers
-# from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 
-# from .models import Worker
-# from .utils import Util
-
-
-
-# """ Serialization for CustomUser Authentification """
-# class UserRegistrationSerializer(serializers.ModelSerializer):
-#   # Ro'yhatdan o'tish vaqtida parolni tekshirish uchun password2 maydoni yaratib olindi
-#   password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
-#   class Meta:
-#     model = CustomUser
-#     fields = (
-#             "password",
-#             "password2", 
-#             "first_name",
-#             "last_name",
-#             "email",
-#             "username",
-#             "passport",
-#             "date_of_bith",
-#             "phone1",
-#             "phone2",
-#             "gender",
-#             "address",
-#         )
-#     extra_kwargs={
-#       'password':{'write_only':True}
-#     }
-
-#   # parollarni validatsiyadan o'tkazish va bir biriga mosligini tekshirib chiqamiz
-#   def validate(self, attrs):
-#     password = attrs.get('password')
-#     password2 = attrs.get('password2')
-#     if password != password2:
-#       raise serializers.ValidationError("Kiritilgan parollar birxil emas !!!")
-#     return attrs
-
-#   def create(self, validate_data):
-#     validate_data.pop('password2') 
-#     return CustomUser.objects.create_user(**validate_data)
+from .models import CustomUser, Worker, Role, Permission
+from .utils import Util
 
 
-# class UserLoginSerializer(serializers.Serializer):
-#     passport = serializers.CharField(max_length=15)
-#     password = serializers.CharField(max_length=128, write_only=True)
 
-#     def validate(self, attrs):
-#         passport = attrs.get('passport')
-#         password = attrs.get('password')
+""" Serialization for CustomUser Authentification """
+class UserRegistrationSerializer(serializers.ModelSerializer):
+  # Ro'yhatdan o'tish vaqtida parolni tekshirish uchun password2 maydoni yaratib olindi
+  password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
+  class Meta:
+    model = CustomUser
+    fields = (
+            "password",
+            "password2", 
+            "first_name",
+            "last_name",
+            "email",
+            "username",
+            "phone1",
+            "phone2",
+        )
+    extra_kwargs={
+      'password':{'write_only':True}
+    }
 
-#         # Foydalanuvchini passport va password orqali autentifikatsiya qilishi kerak
-#         user = authenticate(passport=passport, password=password)
-#         if not user:
-#             raise serializers.ValidationError("Passport yoki parol noto'g'ri !")
-#         attrs['user'] = user
-#         return attrs
+  # parollarni validatsiyadan o'tkazish va bir biriga mosligini tekshirib chiqamiz
+  def validate(self, attrs):
+    password = attrs.get('password')
+    password2 = attrs.get('password2')
+    if password != password2:
+      raise serializers.ValidationError("Kiritilgan parollar birxil emas !!!")
+    return attrs
+
+  def create(self, validate_data):
+    validate_data.pop('password2') 
+    return CustomUser.objects.create_user(**validate_data)
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=15)
+    password = serializers.CharField(max_length=128, write_only=True)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        # Foydalanuvchini username va password orqali autentifikatsiya qilishi kerak
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise serializers.ValidationError("Username yoki parol noto'g'ri !")
+        attrs['user'] = user
+        return attrs
       
         
-# class LogoutSerializer(serializers.Serializer):
-#     refresh = serializers.CharField()
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
 
-#     # Tokenni blacklist ga qo'shish
-#     def validate(self, attrs):
-#         self.token = attrs['refresh']
-#         return attrs
+    # Tokenni blacklist ga qo'shish
+    def validate(self, attrs):
+        self.token = attrs['refresh']
+        return attrs
 
-#     def save(self, **kwargs):
-#         try:
-#             token = RefreshToken(self.token)
-#             token.blacklist()
-#         except Exception as e:
-#             raise serializers.ValidationError("Tokenni blacklist ga qo'shib bo'lmadi, yoki bu token avval ishlatilgan !")
+    def save(self, **kwargs):
+        try:
+            token = RefreshToken(self.token)
+            token.blacklist()
+        except Exception as e:
+            raise serializers.ValidationError("Tokenni blacklist ga qo'shib bo'lmadi, yoki bu token avval ishlatilgan !")
       
 
-# class UserChangePasswordSerializer(serializers.Serializer):
-#   current_password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
-#   password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
-#   password2 = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
+class UserChangePasswordSerializer(serializers.Serializer):
+  current_password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
+  password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
+  password2 = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
 
-#   class Meta:
-#     fields = ['current_password', 'password', 'password2']
+  class Meta:
+    fields = ['current_password', 'password', 'password2']
 
-#   def validate_current_password(self, value):
-#     user = self.context.get('user')
-#     if not user.check_password(value):
-#       raise serializers.ValidationError("Joriy parol noto'g'ri kiritildi !")
-#     return value
+  def validate_current_password(self, value):
+    user = self.context.get('user')
+    if not user.check_password(value):
+      raise serializers.ValidationError("Joriy parol noto'g'ri kiritildi !")
+    return value
 
-#   def validate(self, attrs):
-#     password = attrs.get('password')
-#     password2 = attrs.get('password2')
-#     if password != password2:
-#       raise serializers.ValidationError("parol muvaffaqiyatli o'zgartirildi !")
-#     user = self.context.get('user')
-#     user.set_password(password)
-#     user.save()
-#     return attrs
+  def validate(self, attrs):
+    password = attrs.get('password')
+    password2 = attrs.get('password2')
+    if password != password2:
+      raise serializers.ValidationError("parol muvaffaqiyatli o'zgartirildi !")
+    user = self.context.get('user')
+    user.set_password(password)
+    user.save()
+    return attrs
 
 
-# class SendPasswordResetEmailSerializer(serializers.Serializer):
-#     email = serializers.EmailField(max_length=255)
-#     class Meta:
-#         fields = ['email']
+class SendPasswordResetEmailSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    class Meta:
+        fields = ['email']
 
-#     def validate(self, attrs):
-#         email = attrs.get('email')
-#         # print(f"attrs ---- {email}")
+    def validate(self, attrs):
+        email = attrs.get('email')
+        # print(f"attrs ---- {email}")
         
-#         if CustomUser.objects.filter(email=email).exists():
-#             user = CustomUser.objects.get(email = email)
-#             # print(f"user ---- {user.email}")
-#             uid = urlsafe_base64_encode(force_bytes(user.id))
-#             # print("--------------------------------------------------------------------------------")
-#             # print('Encoded UID', uid)
-#             token = PasswordResetTokenGenerator().make_token(user)
-#             # print('Password Reset Token', token)
-#             link = 'http://localhost:3000/api/user/reset/'+uid+'/'+token
-#             # print('Password Reset Link', link)
-#             # print("-------------------------------------------------------------------------------")
-#             # Send EMail
-#             body = 'Parolingizni tiklash uchun quyidagi havolani bosing '+link
-#             data = {
-#                 'subject':'Reset Your Password',
-#                 'body':body,
-#                 'to_email':user.email
-#             }
-#             Util.send_email(data)
-#             return attrs
-#         else:
-#             raise serializers.ValidationError("Siz ro'yhatdan o'tmagansiz")
+        if CustomUser.objects.filter(email=email).exists():
+            user = CustomUser.objects.get(email = email)
+            # print(f"user ---- {user.email}")
+            uid = urlsafe_base64_encode(force_bytes(user.id))
+            # print("--------------------------------------------------------------------------------")
+            # print('Encoded UID', uid)
+            token = PasswordResetTokenGenerator().make_token(user)
+            # print('Password Reset Token', token)
+            link = 'http://localhost:3000/api/user/reset/'+uid+'/'+token
+            # print('Password Reset Link', link)
+            # print("-------------------------------------------------------------------------------")
+            # Send EMail
+            body = 'Parolingizni tiklash uchun quyidagi havolani bosing '+link
+            data = {
+                'subject':'Reset Your Password',
+                'body':body,
+                'to_email':user.email
+            }
+            Util.send_email(data)
+            return attrs
+        else:
+            raise serializers.ValidationError("Siz ro'yhatdan o'tmagansiz")
 
 
-# class UserPasswordResetSerializer(serializers.Serializer):
-#     password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
-#     password2 = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
-#     class Meta:
-#         fields = ['password', 'password2']
+class UserPasswordResetSerializer(serializers.Serializer):
+    password = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
+    password2 = serializers.CharField(max_length=255, style={'input_type':'password'}, write_only=True)
+    class Meta:
+        fields = ['password', 'password2']
 
-#     def validate(self, attrs):
-#         try:
-#             password = attrs.get('password')
-#             password2 = attrs.get('password2')
-#             uid = self.context.get('uid')
-#             token = self.context.get('token')
-#             if password != password2:
-#                 raise serializers.ValidationError("Password and Confirm Password doesn't match")
-#             id = smart_str(urlsafe_base64_decode(uid))
-#             user = Student.objects.get(id=id)
-#             if not PasswordResetTokenGenerator().check_token(user, token):
-#                 raise serializers.ValidationError('Token is not Valid or Expired')
-#             user.set_password(password)
-#             user.save()
-#             return attrs
-#         except DjangoUnicodeDecodeError as identifier:
-#             PasswordResetTokenGenerator().check_token(user, token)
-#             raise serializers.ValidationError('Token is not Valid or Expired')
+    def validate(self, attrs):
+        try:
+            password = attrs.get('password')
+            password2 = attrs.get('password2')
+            uid = self.context.get('uid')
+            token = self.context.get('token')
+            if password != password2:
+                raise serializers.ValidationError("Password and Confirm Password doesn't match")
+            id = smart_str(urlsafe_base64_decode(uid))
+            user = CustomUser.objects.get(id=id)
+            if not PasswordResetTokenGenerator().check_token(user, token):
+                raise serializers.ValidationError('Token is not Valid or Expired')
+            user.set_password(password)
+            user.save()
+            return attrs
+        except DjangoUnicodeDecodeError as identifier:
+            PasswordResetTokenGenerator().check_token(user, token)
+            raise serializers.ValidationError('Token is not Valid or Expired')
      
   
   
   
-# """ ---------------- Serialization for User ---------------- """
+""" ---------------- Serialization for User ---------------- """
   
-# class UserProfileSerializer(serializers.ModelSerializer): 
-#     """ User login bo'lganda, tokenga qo'shimcha ravishda uning ma'lumotlarini yuborish uchun serializer """
+class UserProfileSerializer(serializers.ModelSerializer): 
+    """ User login bo'lganda, tokenga qo'shimcha ravishda uning ma'lumotlarini yuborish uchun serializer """
     
-#     class Meta:
-#         model = CustomUser
-#         fields = (
-#             "id",
-#             "passport",
-#             "date_of_bith",
-#             "phone1",
-#             "phone2",
-#             "gender",
-#             "personal_status",
-#             "address",
-#             "status",
-#             "created_on",
-#         )
+    class Meta:
+        model = CustomUser
+        fields = (
+            "id",
+            "phone1",
+            "phone2",
+        )
     
   
 # class CustomUser_Create_Serializer(serializers.ModelSerializer):
--- accaunt/views.py
+++ accaunt/views.py
@@ -1,111 +1,111 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 
-# # Create your views here.
-# from django.contrib.auth import authenticate
-# from pprint import pprint
+# Create your views here.
+from django.contrib.auth import authenticate
+from pprint import pprint
 
-# from rest_framework.views import APIView
-# from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
-# from rest_framework.response import Response
-# from rest_framework import status, generics
-# from rest_framework_simplejwt.tokens import RefreshToken
-# from rest_framework import viewsets
-
-
-# from .renderers import UserRenderer
-# from .models import Worker
-
-# from .serializers import \
-#     UserRegistrationSerializer,\
-#         UserLoginSerializer,\
-#         LogoutSerializer,\
-#         UserChangePasswordSerializer,\
-#         SendPasswordResetEmailSerializer,\
-#         UserPasswordResetSerializer,\
-#         UserProfileSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework import status, generics
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import viewsets
 
 
+from .renderers import UserRenderer
+from .models import CustomUser
 
-# """ Viewsets for User Authentication """
-# def get_tokens_for_user(user):
-#     refresh = RefreshToken.for_user(user)
-#     return {
-#         'refresh': str(refresh),
-#         'access': str(refresh.access_token),
-#     }
-
-# class UserRegistrationView(APIView):
-#     # renderer_classes = [UserRenderer]
-
-#     def post(self, request, format=None):
-#         serializer = UserRegistrationSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         user = serializer.save()
-#         token = get_tokens_for_user(user)
-#         return Response({'token': token, 'message': "Ro'yhatdan muvaffaqiyatli o'tdingiz"}, status=status.HTTP_201_CREATED)
+from .serializers import \
+    UserRegistrationSerializer,\
+        UserLoginSerializer,\
+        LogoutSerializer,\
+        UserChangePasswordSerializer,\
+        SendPasswordResetEmailSerializer,\
+        UserPasswordResetSerializer,\
+        UserProfileSerializer
 
 
-# class UserLoginView(APIView):
-#     def post(self, request, *args, **kwargs):
-#         serializer = UserLoginSerializer(data=request.data)
+
+""" Viewsets for User Authentication """
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+class UserRegistrationView(APIView):
+    # renderer_classes = [UserRenderer]
+
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token = get_tokens_for_user(user)
+        return Response({'token': token, 'message': "Ro'yhatdan muvaffaqiyatli o'tdingiz"}, status=status.HTTP_201_CREATED)
+
+
+class UserLoginView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserLoginSerializer(data=request.data)
              
-#         if serializer.is_valid():
-#             user = serializer.validated_data['user']
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
 
-#             """ Login bo'lgan userning ma'lumotlarini bazadan passporti bo'yicha olamiz  """
-#             active_user = CustomUser.objects.get(passport=serializer.data['passport'])
-#             active_user_serializer = UserProfileSerializer(active_user)
+            """ Login bo'lgan userning ma'lumotlarini bazadan username bo'yicha olamiz  """
+            active_user = CustomUser.objects.get(username=serializer.data['username'])
+            active_user_serializer = UserProfileSerializer(active_user)
             
-#             refresh = RefreshToken.for_user(user)
+            refresh = RefreshToken.for_user(user)
             
-#             return Response({
-#                 "refresh": str(refresh),
-#                 "access": str(refresh.access_token),
-#                 "user":active_user_serializer.data # userning shasiy ma'lumotlari
-#             }, status=status.HTTP_200_OK)
+            return Response({
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+                "user":active_user_serializer.data # userning shasiy ma'lumotlari
+            }, status=status.HTTP_200_OK)
             
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# class LogoutAPIView(APIView):
-#     permission_classes = [IsAuthenticated]
+class LogoutAPIView(APIView):
+    permission_classes = [IsAuthenticated]
 
-#     def post(self, request, *args, **kwargs):
-#         serializer = LogoutSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response({"message": "Tizimdan muvaffaqiyatli chiqdingiz !"}, status=status.HTTP_200_OK)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-# class UserChangePasswordView(APIView):
-#     renderer_classes = [UserRenderer]
-#     permission_classes = [IsAuthenticated]
-
-#     def post(self, request, format=None):
-#         serializer = UserChangePasswordSerializer(
-#             data=request.data, context={'user': request.user})
-#         serializer.is_valid(raise_exception=True)
-#         return Response({'message': "Parol muvaffaqiyatli o'zgartirildi"}, status=status.HTTP_200_OK)
+    def post(self, request, *args, **kwargs):
+        serializer = LogoutSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Tizimdan muvaffaqiyatli chiqdingiz !"}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# class SendPasswordResetEmailView(APIView):
-#     renderer_classes = [UserRenderer]
+class UserChangePasswordView(APIView):
+    renderer_classes = [UserRenderer]
+    permission_classes = [IsAuthenticated]
 
-#     def post(self, request, format=None):
-#         serializer = SendPasswordResetEmailSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         return Response({'message': "Parolni tiklash uchun link yuborildi. Iltimos emailingizni tekshiring"}, status=status.HTTP_200_OK)
+    def post(self, request, format=None):
+        serializer = UserChangePasswordSerializer(
+            data=request.data, context={'user': request.user})
+        serializer.is_valid(raise_exception=True)
+        return Response({'message': "Parol muvaffaqiyatli o'zgartirildi"}, status=status.HTTP_200_OK)
 
 
-# class UserPasswordResetView(APIView):
-#     renderer_classes = [UserRenderer]
+class SendPasswordResetEmailView(APIView):
+    renderer_classes = [UserRenderer]
 
-#     def post(self, request, uid, token, format=None):
-#         serializer = UserPasswordResetSerializer(
-#             data=request.data, context={'uid': uid, 'token': token})
-#         serializer.is_valid(raise_exception=True)
-#         return Response({'message': 'Parol muvaffaqiyatli yangilandi'}, status=status.HTTP_200_OK)
+    def post(self, request, format=None):
+        serializer = SendPasswordResetEmailSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response({'message': "Parolni tiklash uchun link yuborildi. Iltimos emailingizni tekshiring"}, status=status.HTTP_200_OK)
+
+
+class UserPasswordResetView(APIView):
+    renderer_classes = [UserRenderer]
+
+    def post(self, request, uid, token, format=None):
+        serializer = UserPasswordResetSerializer(
+            data=request.data, context={'uid': uid, 'token': token})
+        serializer.is_valid(raise_exception=True)
+        return Response({'message': 'Parol muvaffaqiyatli yangilandi'}, status=status.HTTP_200_OK)
 
 
 
